a7ebb9f01301e46f59e8f5001414818cb7e42179
Svetlana184 <sveta.kgd.39@gmail.com>
2025-11-18 11:02:04 +0200

    array types, multidimensional arrays

--- lesson3.ts
+++ lesson3.ts
@@ -1,7 +1,11 @@
+//3 - arrays
+
+
 //ARRAY TYPES
 const numbers : number[] = [1,2,3,4];
 const names : Array<string> = ["Alice", "Bob"];  //устаревшее написание
 //const error_array : number[] = [1,2,3,"Bob"]; - error
+
 
 //MULTIDIMENTIONAL ARRAYS
 const matrix: number[][] = [
